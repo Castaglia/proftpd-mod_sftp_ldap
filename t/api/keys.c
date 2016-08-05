5b1088e04baf4a5105c0b499fd509ca032f85ddd
TJ Saunders <tj@castaglia.org>
2016-08-05 13:59:20 -0700

    Start working on enabling travis-ci support, for running unit tests, which I can then use for developing better key-parsing routines for mod_ldap-provided SSH public keys.

---
+++ t/api/keys.c
@@ -0,0 +1,73 @@
+/*
+ * ProFTPD - mod_sftp_ldap testsuite
+ * Copyright (c) 2016 TJ Saunders <tj@castaglia.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Suite 500, Boston, MA 02110-1335, USA.
+ *
+ * As a special exemption, TJ Saunders and other respective copyright holders
+ * give permission to link this program with OpenSSL, and distribute the
+ * resulting executable, without including the source code for OpenSSL in the
+ * source distribution.
+ */
+
+/* Key-parsing tests. */
+
+#include "tests.h"
+
+static pool *p = NULL;
+
+static void set_up(void) {
+  if (p == NULL) {
+    p = make_sub_pool(NULL);
+  }
+
+  if (getenv("TEST_VERBOSE") != NULL) {
+  }
+}
+
+static void tear_down(void) {
+  if (getenv("TEST_VERBOSE") != NULL) {
+  }
+
+  if (p) {
+    destroy_pool(p);
+    p = NULL;
+  } 
+}
+
+START_TEST (raw_key_test) {
+}
+END_TEST
+
+START_TEST (rfc4716_key_test) {
+  /* One key WITH Comment/Subject headers (fail), one key without */
+}
+END_TEST
+
+Suite *tests_get_keys_suite(void) {
+  Suite *suite;
+  TCase *testcase;
+
+  suite = suite_create("keys");
+  testcase = tcase_create("base");
+
+  tcase_add_checked_fixture(testcase, set_up, tear_down);
+
+  tcase_add_test(testcase, raw_key_test);
+  tcase_add_test(testcase, rfc4716_key_test);
+
+  suite_add_tcase(suite, testcase);
+  return suite;
+}
